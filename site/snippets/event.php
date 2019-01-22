8f41dc23497259cf8dd0524bc90e5e2d12a10119
augcouec <augcouec@gmail.com>
2019-01-22 09:15:21 +0100

    new card

--- site/snippets/event.php
+++ site/snippets/event.php
@@ -1,3 +1,13 @@
+<?php
+
+function getCatColor($cat, $site) {
+    $categoryPage = $site->page("categories")->children()->findBy("slug", $cat);
+    // $color = $categoryPage->content->get(‘color’);
+    // var_dump($color); die;
+}
+
+?> 
+
 <?php // Fetch all events ?>
 <?php $events = $page->children()->visible(); ?>
 
@@ -5,24 +15,46 @@
 
     <div class="event all-categories ">
 
-        <div class="event__title">
-            <h2 class="event__title__font">
-                <?= $event->title()->html() ?>
-            </h2>
-        </div>
+         <div class="event__info">
 
+    <?php if($date = $event->content()->get('date')): ?>
+                    <?php $year = date('Y', strtotime($date)); ?>
+                    <?php $month_day = date('F d', strtotime($date)); ?>
 
-        <?php if ($date = $event->content()->get('date')) : ?>
-            <div class="event__date">
-                <?= html($date) ?>
+            <div class="event__date__year">
+                <?= html($year) ?>
+
             </div>
-        <?php endif ?>
+            <div class="event__date__month">
+                <?= html($month_day) ?>
 
+            </div>
+            
+        <?php endif ?>
+        
         <?php if ($category = $event->content()->get('category')) : ?>
             <div class="event__category">
                 <?= html($category) ?>
+                 <?php getCatColor($category,$site) ?>
+
             </div>
         <?php endif ?>
+
+                 </div>
+
+<div class="event__card">
+
+
+
+        <div class="event__title">
+            <h2 class="event__title">
+                <?= $event->title()->html() ?>
+            </h2>
+            <hr>
+        </div>
+  
+
+ 
 
                 <?php if ($content = $event->content()->get('content')) : ?>
             <div class="event__content">
@@ -36,7 +68,7 @@
             </div>
         <?php endif ?> -->
 
-
+</div>
 
     </div>
 
